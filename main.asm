e4145a61bd08846310df8e8ba5ca7eb41b8dc569
aymanElakwah <ayman.elakwah@gmail.com>
2018-11-29 00:36:53 +0200

    Change main to test movePlayer

--- main.asm
+++ main.asm
@@ -1,3 +1,5 @@
+include MovePlayer.inc
+include akwah.inc
 data segment
 
 player1 label byte
@@ -10,13 +12,16 @@ player2y db ?
 player2Ori db ?
 
 pnum db  ?          ;Player number = pnum (0 or 1)
+generalPlayer label byte
 posx  db  ?        ;Positon x of general player
 posy db  ?        ;Positon y of general player
 ori  db  ?          ;Orintation of player: Right = 16, Left = 17, Up = 30, Down = 31
 
 conf  db 16,17,30,31
-chngx db 01,-1,01,-1
-chngy db 00,00,00,00
+chngx db 01,-1,00,00
+chngy db 00,00,-1,01
+
+message db 300 dup(' '), '$'
 
 ends
 
@@ -29,9 +34,21 @@ start:
 ; set segment registers:
     mov ax, data
     mov ds, ax
-    mov es, ax
+    
+    mov dx, offset message
+    mov ah, 9
+    int 21h
+    mov dx, 0
+    mov ah, 2
+    int 10h
+    mov pnum, 1
+    mov player1ori, 16
+    mov ori, 16
+    DrawPlayer
     ; add your code here
-            
+    label:
+    mainGameLoop
+    jmp label        
     
     
     mov ax, 4c00h ; exit to operating system.
