337690dd4440c8fecb9945dad7452269bd1d3d52
aymanElakwah <33968577+aymanElakwah@users.noreply.github.com>
2018-11-28 20:22:42 +0200

    Create main.asm

---
+++ main.asm
@@ -0,0 +1,23 @@
+data segment
+    
+ends
+
+stack segment
+    dw   128  dup(0)
+ends
+
+code segment
+start:
+; set segment registers:
+    mov ax, data
+    mov ds, ax
+    mov es, ax
+    ; add your code here
+            
+    
+    
+    mov ax, 4c00h ; exit to operating system.
+    int 21h    
+ends
+
+end start ; set entry point and stop the assembler.
