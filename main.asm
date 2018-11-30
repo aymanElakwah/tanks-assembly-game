d7380d8202ccf852d00050361acfa02521f974a8
aymanElakwah <ayman.elakwah@gmail.com>
2018-11-30 11:42:02 +0200

    Map, fire bullets with delay, build walls

--- main.asm
+++ main.asm
@@ -1,15 +1,20 @@
 include MovePlayer.inc
 include akwah.inc
-data segment
+include Macros.inc
+include blt.inc
+.model large
 
+data segment 
+    
 player1 label byte
-player1x db 0
+player1x db 21
 player1y db 0
 player1Ori db 16
 player2 label byte
 player2x db 76
 player2y db 22
 player2Ori db 17
+
 
 pnum db  ?          ;Player number = pnum (0 or 1)
 generalPlayer label byte
@@ -21,7 +26,13 @@ conf  db 16,17,30,31
 chngx db 01,-1,00,00
 chngy db 00,00,-1,01
 
-message db 2000 dup(' '), '$'
+blt1 db 3 dup(0ffh, ?, ?)
+blt2 db 3 dup(0FFh, ?, ?)  
+
+message db 0 dup(' '), '$'
+uppermap db '                 ³Ú²²                                                                            ³Û²²                ²²     ²²²²²²²²            ²²²²²²²² ²  ²²                   ³²²²          ²²²         ²²²²²²²²²²   ²²²²²²  ²²²²²²²² ²    ²²                 ³²²²   ²²²²  ²² ²        ²²²²²²²²²²²²  ²²²²²²  ²²       ²                       ³     ²         ²²      ²²²²²²²²²²²²²² ²²²²²²    ²²²    ²   ²²                  ³   ²²   ²²² ²² ²²     ²²²²²²²²²²²²²²² ²²²²²²    ²²²    ²                       ³        ²²² ²² ²²     ²²²²²²²²²²²²²²² ²²²²²²       ²²² ²  ²²                   ³ ²²²²   ²²²    ²   ²² ²²²²²²²²²²²²²²²              ²                           ³   ²           ²      ²²²  ²²²²  ²²²²         ²²²²²²     ²²                    ³  ²      ²²²²²²   ²²  ²²   ²²²²   ²²²            ²²²²    ²²                    ³²²   ²²²²             ²    ²²²²    ²²            ²²²²  ²²     ÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ³²²²²²²²²  ²   ²²      ²²²²²²²²²²²²²²² ²²²²²²      ²²²  ²²     '
+lowermap db '                 ³²²  ²²²       ²   ²   ²²²²²²²²²²²²²²²         ²²²²²²²² ²                       ³ ²     ²²²²²²           ²²²²²²²²²²²           ²²²²²²²²²²  ²²                   ³ ²²    ²²²²²²²          ² ²²²²²² ²²        ²²²²          ²²                    ³ ²²    ²²²²²²²²    ²²   ²²²²²²²²²²²       ²²     ²²   ²²                       ³  ²            ²²    ²² ²²²²²²²²²²²      ²²²²        ²²                        ³   ²²       ²   ²²      ² ² ² ² ² ²      ²²   ²²    ²²                         ³    ²²²  ²   ²    ²²    ² ² ² ² ² ²     ²²    ²²   ²²                          ³    ²  ²² ²  ²²    ²²²²  ² ² ² ² ²    ²²   ²²    ²²                            ³   ²          ²      ²²              ²²    ²²              ²²²                 ³ ²²   ² ² ²² ²         ²²²²²²²²²²²²²²                      ²²²                 ³                                                           ²²Ú                 ³²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²² ²²Û$'
+temp db 0
 
 ends
 
@@ -32,8 +43,16 @@ ends
 code segment
 start:
 ; set segment registers:
-    mov ax, data
+    mov ax, @data
     mov ds, ax
+    mov al, 3
+    mov ah, 0
+    int 10h
+    mov ch, 32
+    mov ah, 1
+    int 10h
+    loadmap 
+     
 
     mov dx, offset message
     mov ah, 9
@@ -47,8 +66,11 @@ start:
     mov bx, offset generalPlayer
     draw
     ; add your code here
+    mov bp, 0ffffh
     label:
+    
     mainGameLoop
+    
     jmp label
 
 
