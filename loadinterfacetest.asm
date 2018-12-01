4bcb0d887cde8524c87a29d18dde635e0a0e2999
Omar Ahmed <37841606+IamADeadFish@users.noreply.github.com>
2018-12-01 11:56:07 +0200

    Add files via upload
    interface , with map cleared

---
+++ loadinterfacetest.asm
@@ -0,0 +1,46 @@
+.model small
+.data
+
+
+
+Startinterface   db    '      _________       _______        _              _              _______      '
+                 db    '      \__   __/      (  ___  )      ( (    /|      | \    /\      (  ____ \     '
+                 db    '         ) (         | (   ) |      |  \  ( |      |  \  / /      | (    \/     '
+                 db    '         | |         | (___) |      |   \ | |      |  (_/ /       | (_____      '
+                 db    '         | |         |  ___  |      | (\ \) |      |   _ (        (_____  )     '
+                 db    '         | |         | (   ) |      | | \   |      |  ( \ \             ) |     '
+                 db    '         | |         | )   ( |      | )  \  |      |  /  \ \      /\____) |     '
+                 db    '         )_(         |/     \|      |/    )_)      |_/    \/      \_______)     '
+                 db    '                                                                                '
+                 db    '                                                                                '
+                 db    '                                                                                '
+                 db    '                                                                                '
+                 db    '                                                                                '
+                 db    '                                                                                '
+                 db    '                                                                                '
+                 db    '                                                                                '
+                 db    '                                                                                ' 
+CHOOSEMODE       db    '                             ',16,' Normal  mode                                    '
+                 db    '                                Network mode (Coming Soon)                          $'
+                 
+                 
+      
+.code
+
+
+
+main proc far
+    
+    mov ax,@data
+    mov ds,ax
+    
+    loadinterface
+    
+    
+    
+    main endp
+end main
+    
+    
+    
+    
